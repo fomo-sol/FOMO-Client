0f1cb5696046a5044a8d000538ae2144ac1fa5c8
kmkkkp <kmkkkp@ajou.ac.kr>
2025-07-14 15:12:34 +0900

    [Fix] global css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav
-      className="text-white text-lg items-center justify-between px-6 py-6 flex gap-4 border-b"
-      style={{ backgroundColor: "#081835", borderBlockColor: "#282C34" }}
+      className="text-lg items-center justify-between px-6 py-6 flex gap-4 border-b"
+      style={{ borderBlockColor: "#282C34" }}
     >
       <Link href="/" className="font-bold text-2xl">
         FOMO
       </Link>
       <div className="flex items-center gap-4">
         <input
-          className="bg-white text-black rounded-2xl focus:outline-none text-center"
+          className=" rounded-2xl focus:outline-none text-center"
           style={{ width: "200px" }}
         ></input>
         <Link href="/fomc">FOMC</Link>
