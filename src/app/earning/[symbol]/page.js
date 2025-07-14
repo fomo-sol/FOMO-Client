3f700f908a65bdf5ee7cde19920f9927d0a879d9
JeongHyeon Jo <cisxo@naver.com>
2025-07-14 22:30:10 +0900

    feat(bymd): Detect chart left edge to trigger && fix view position

--- src/app/earning/[symbol]/page.js
+++ src/app/earning/[symbol]/page.js
@@ -29,7 +29,7 @@ export default function EarningReleasePage() {
       <div className="p-6">
         <h1 className="text-2xl font-bold mb-6">Chart for {symbol}</h1>
         <div style={{ display: "flex", justifyContent: "flex-start" }}>
-          <StockChart stockData={stockData} />
+          <StockChart stockData={stockData} symbol={symbol} />
         </div>
       </div>
   );
