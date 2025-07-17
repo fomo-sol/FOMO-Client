0e63e3e8d1fb32ebf7405e54d55a74c27cb0d1e6
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2025-07-17 10:20:06 +0900

    [Fix] tokenInitializer 제거

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,6 @@ export default function RootLayout({ children }) {
     <html lang="ko" className={`${pretendard.variable} `}>
       <body className="">
         <Navbar />
-        <TokenInitializer />
         {children}
       </body>
     </html>
