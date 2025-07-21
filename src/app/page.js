f650b66305ff88c572c8e4d8465302eaff13a705
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2025-07-21 16:26:49 +0900

    [Feat] 메인 페이지 생성

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,31 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
+import useAuth from "../../utils/useAuth";
 
 export default function Home() {
+  const router = useRouter();
+  const isLoggedIn = useAuth();
+
+  const handleStart = () => {
+    if (isLoggedIn) {
+      router.push("/calendar");
+    } else {
+      router.push("/login");
+    }
+  };
+
   return (
-    <div className="items-center justify-items-center pt-10">
-      <img src="/FOMO.png" alt="FOMO Logo" />
-      제목은 왼쪽이랑 위 padding 6으로만 맞추고 개발하면 될듯 합니다 <br />
-      이하 코드 작성 고고! (grid로 가로축 나누는 등)
+    <div className="min-h-screen bg-[#040816] flex flex-col items-center justify-start text-center pt-40">
+      <h2 className="text-white text-4xl md:text-5xl font-medium mb-8">
+        발표가 끝나면 바로 도착하는 한글 요약
+      </h2>
+      <h1 className="text-white text-7xl md:text-8xl font-semibold mb-10">FOMO</h1>
+      <button
+        className="bg-white text-[#040816] text-2xl font-semibold rounded-xl px-12 py-4 shadow hover:bg-gray-200 transition"
+        onClick={handleStart}
+      >
+        시작하기
+      </button>
     </div>
   );
 }
